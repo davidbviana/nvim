948f8b7cfcb71373ee628aae6cd77fd640dc71ba
David Viana <me@davidviana.com>
2024-11-15 23:26:51 +0000

    Add tentative install script

---
+++ install.sh
@@ -0,0 +1,6 @@
+sudo apt-get update && sudo apt-get install neovim
+mkdir -p ~/.config/nvim
+cp init.vim ~/.config/nvim/init.vim
+mkdir -p ~/.config/nvim/autoload
+cp plug.vim ~/.config/nvim/autoload/plug.vim
+nvim +'PlugInstall --sync' +qa
