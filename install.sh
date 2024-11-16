9ee76966cee150b0868327fc8c5f99250fb0d1de
davidbviana <133513890+davidbviana@users.noreply.github.com>
2024-11-16 19:10:33 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-sudo apt-get update && sudo apt-get install neovim
+
+sudo apt-get update && sudo apt-get -y install neovim
+
+temp_dir=$(mktemp -d)
+git clone https://github.com/davidbviana/nvim.git "$temp_dir"
+cd "$temp_dir"
+
 mkdir -p ~/.config/nvim
 cp init.vim ~/.config/nvim/init.vim
 mkdir -p ~/.config/nvim/autoload
